3c1603a68b6c98ae6163d083dc54e55f2cd5e206
unknown <arash.kamandaar86@gmail.com>
2025-08-21 13:45:45 +0330

    Address card minor css changes

--- src/components/AddressCard.jsx
+++ src/components/AddressCard.jsx
@@ -2,34 +2,46 @@ import { MapComponent } from "./MapComponent"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faExternalLink, faTrashCan } from "@fortawesome/free-solid-svg-icons"
 import { useState } from "react"
+import greenbg from "../images/greenbg.svg"
+
 export function AddressCard({ title, address, number, lng, lat }) {
     const [showMap, setShowMap] = useState(false)
 
 
     return (
-        <div onClick={() => setShowMap((s) => !s)} className={`flex relative flex-col ${showMap ? "border border-green-500" : "border"} rounded-xl gap-2 cursor-pointer group`}>
+        <div onClick={() => setShowMap((s) => !s)} className={`flex relative flex-col group ${showMap ? "border border-green-500" : "border"} rounded-xl gap-2 cursor-pointer group`}>
             <div className="flex justify-between items-center ">
                 <div className="flex items-center justify-between w-full gap-4 rounded-xl  mr-0 pr-0  h-full relative">
-                    <div className={"flex items-center gap-6 bg-white h-full rounded-xl w-[90%] py-6"}>
-                        <div className=" rounded-l-full text-center flex items-center justify-start">
-                            <div style={{
-                                clipPath: 'polygon(74% 9%, 100% 0, 100% 100%, 74% 90%)'
-                            }} className="bg-green-500 size-10 rounded-r-sm"></div>
-                            <div style={{
-                                right: '0.3rem'
-                            }} 
-                            className="h-8 absolute w-12 rounded-l-full rounded-r-lg bg-green-500 text-white flex items-center justify-center">
-                                <p className="text-2xl ">{number}</p>
+                    <div className={`flex items-center justify-between bg-white h-full rounded-xl w-full hover:w-[90%] transition-all duration-300 py-[1.3rem] ${!showMap && "group-active:border-[3px] group-active:border-green-500 group-active:duration-75 group-active:w-[90%]"} `}>
+                        <div className="flex justify-start items-center gap-2">
+                            <div className="  pr-0">
+                                <div className="flex items-center justify-start relative">
+                                    <div
+                                        style={{
+                                            backgroundImage: `url(${greenbg})`,
+                                            backgroundRepeat: "no-repeat",
+                                            backgroundPosition: "center",
+                                            backgroundSize: "contain",
+                                            
+                                        }}
+                                        className="h-10 w-14 flex items-center justify-center relative text-white text-lg right-[-11px]"
+                                    >
+                                        {number}
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className="relative right-[-15px]">
+                                <h2 className="text-gray-700 text-xl">{title}</h2>
+                                <p className="text-gray-400">{address}</p>
                             </div>
                         </div>
-                        <div>
-                            <h2 className="text-gray-700 text-xl">{title}</h2>
-                            <p className="text-gray-400">{address}</p>
-                        </div>
+                        <FontAwesomeIcon className={`text-green-500 z-50 pl-6 delay-200 ${!showMap && " group-hover:hidden group-hover:delay-1000"} ${showMap && "hidden"}`} icon={faExternalLink} />
                     </div>
                     <div className={`p-4 md:pl-6 rounded-xl flex justify-end items-center gap-2 w-[100%] h-full absolute -z-10 
                     ${!showMap && "group-hover:bg-green-500"}  transition-colors`}>
-                        <FontAwesomeIcon className={`text-green-500 ${!showMap && "group-hover:text-white"}`} icon={faExternalLink} />
+                        <FontAwesomeIcon className={`text-green-500 z-50 relative ${!showMap && "group-hover:text-white"}`} icon={faExternalLink} />
                         {
                             showMap && (
                                 <FontAwesomeIcon className={`text-green-500 ${!showMap && "group-hover:text-white"} `} icon={faTrashCan} />
